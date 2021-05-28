4df8bcbc12404bee0c6e6b1518c5d12b0ebc1327
Danasatria <68360967+Danasatria@users.noreply.github.com>
2021-05-28 21:10:25 +0700

    Dashboard layout

--- resources/views/master/navbar/sidebar.blade.php
+++ resources/views/master/navbar/sidebar.blade.php
@@ -19,7 +19,7 @@
         </div>
         <ul class="navbar-nav flex-fill w-100 mb-2">
             <li class="nav-item w-100">
-                <a class="nav-link" href="calendar.html">
+                <a class="nav-link active" href="#">
                     <i class="fe fe-home fe-16"></i>
                     <span class="ml-3 item-text">Dashboard</span>
                 </a>
@@ -36,34 +36,8 @@
                 </a>
                 <ul class="collapse list-unstyled pl-4 w-100" id="ui-elements">
                     <li class="nav-item">
-                        <a class="nav-link pl-3" href="./ui-color.html"><span class="ml-1 item-text">Colors</span>
+                        <a class="nav-link pl-3" href="./ui-color.html"><span class="ml-1 item-text">Kosong</span>
                         </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link pl-3" href="./ui-typograpy.html"><span
-                                class="ml-1 item-text">Typograpy</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link pl-3" href="./ui-icons.html"><span class="ml-1 item-text">Icons</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link pl-3" href="./ui-buttons.html"><span
-                                class="ml-1 item-text">Buttons</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link pl-3" href="./ui-notification.html"><span
-                                class="ml-1 item-text">Notifications</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link pl-3" href="./ui-modals.html"><span class="ml-1 item-text">Modals</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link pl-3" href="./ui-tabs-accordion.html"><span class="ml-1 item-text">Tabs &
-                                Accordion</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link pl-3" href="./ui-progress.html"><span
-                                class="ml-1 item-text">Progress</span></a>
                     </li>
                 </ul>
             </li>
@@ -73,11 +47,7 @@
                     <span class="ml-3 item-text">Akademik</span>
                 </a>
                 <ul class="collapse list-unstyled pl-4 w-100" id="profile">
-                    <a class="nav-link pl-3" href="./profile.html"><span class="ml-1">Overview</span></a>
-                    <a class="nav-link pl-3" href="./profile-settings.html"><span class="ml-1">Settings</span></a>
-                    <a class="nav-link pl-3" href="./profile-security.html"><span class="ml-1">Security</span></a>
-                    <a class="nav-link pl-3" href="./profile-notification.html"><span
-                            class="ml-1">Notifications</span></a>
+                    <a class="nav-link pl-3" href="./profile.html"><span class="ml-1">Kosong</span></a>
                 </ul>
             </li>
             <li class="nav-item dropdown">
@@ -87,28 +57,16 @@
                 </a>
                 <ul class="collapse list-unstyled pl-4 w-100" id="forms">
                     <li class="nav-item">
-                        <a class="nav-link pl-3" href="./form_elements.html"><span class="ml-1 item-text">Basic
-                                Elements</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link pl-3" href="./form_advanced.html"><span class="ml-1 item-text">Advanced
-                                Elements</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link pl-3" href="./form_validation.html"><span
-                                class="ml-1 item-text">Validation</span></a>
+                        <a class="nav-link pl-3" href="./form_validation.html"><i class="fe fe-plus fe-16 text-success"></i><span
+                                class="ml-1 item-text">Tambah Data</span></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link pl-3" href="./form_wizard.html"><span
-                                class="ml-1 item-text">Wizard</span></a>
+                                class="ml-1 item-text">Data Guru</span></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link pl-3" href="./form_layouts.html"><span
-                                class="ml-1 item-text">Layouts</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link pl-3" href="./form_upload.html"><span class="ml-1 item-text">File
-                                upload</span></a>
+                                class="ml-1 item-text">Data Staff</span></a>
                     </li>
                 </ul>
             </li>
@@ -119,16 +77,11 @@
                 </a>
                 <ul class="collapse list-unstyled pl-4 w-100" id="tables">
                     <li class="nav-item">
-                        <a class="nav-link pl-3" href="./table_basic.html"><span class="ml-1 item-text">Basic
-                                Tables</span></a>
+                        <a class="nav-link pl-3" href="./form_validation.html"><i class="fe fe-plus fe-16 text-success"></i><span
+                                class="ml-1 item-text">Tambah Data</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link pl-3" href="./table_advanced.html"><span class="ml-1 item-text">Advanced
-                                Tables</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link pl-3" href="./table_datatables.html"><span class="ml-1 item-text">Data
-                                Tables</span></a>
+                        <a class="nav-link pl-3" href="./table_datatables.html"><span class="ml-1 item-text">Data Siswa</span></a>
                     </li>
                 </ul>
             </li>
@@ -139,19 +92,7 @@
                 </a>
                 <ul class="collapse list-unstyled pl-4 w-100" id="charts">
                     <li class="nav-item">
-                        <a class="nav-link pl-3" href="./chart-inline.html"><span class="ml-1 item-text">Inline
-                                Chart</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link pl-3" href="./chart-chartjs.html"><span
-                                class="ml-1 item-text">Chartjs</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link pl-3" href="./chart-apexcharts.html"><span
-                                class="ml-1 item-text">ApexCharts</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link pl-3" href="./datamaps.html"><span class="ml-1 item-text">Datamaps</span></a>
+                        <a class="nav-link pl-3" href="./chart-inline.html"><span class="ml-1 item-text">Kosong</span></a>
                     </li>
                 </ul>
             </li>
