0ffacf42e43e893e2390358c3604ae6babe3b909
dio selvinus silalebit <dselvinus@gmail.com>
2021-05-29 02:05:42 +0700

    mebetulkan minor bug profil tidak bisa dibuka

---
+++ resources/views/page/tabel.blade.php
@@ -0,0 +1,69 @@
+@extends('master.master')
+@section('title', 'Data Guru')
+
+@section('content')
+    <div class="row my-4">
+        <!-- Small table -->
+        <div class="col-md-12">
+            <div class="card shadow">
+                <div class="card-body">
+                    <!-- table -->
+                    <table class="table datatables" id="dataTable-1">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>NIP</th>
+                                <th>NAMA</th>
+                                <th>JABATAN</th>
+                                <th>ACTION</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <div class="custom-control custom-checkbox">
+                                        <input type="checkbox" class="custom-control-input">
+                                        <label class="custom-control-label"></label>
+                                    </div>
+                                </td>
+                                <td>123123412312</td>
+                                <td>Aminudin</td>
+                                <td>Guru</td>
+                                <td>
+                                    <button class="btn btn-sm dropdown-toggle more-horizontal" type="button"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <span class="text-muted sr-only">Action</span>
+                                    </button>
+                                    <div class="dropdown-menu dropdown-menu-left">
+                                        <a class="dropdown-item" href="#">Edit</a>
+                                        <a class="dropdown-item" href="#">Remove</a>
+                                        <a class="dropdown-item" href="#">Assign</a>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('css')
+    <link rel="stylesheet" href="{{ asset('css/dataTables.bootstrap4.css') }}">
+@endpush
+
+@push('js')
+    <script src='{{ asset('js/jquery.dataTables.min.js') }}'></script>
+    <script src='{{ asset('js/dataTables.bootstrap4.min.js') }}'></script>
+    <script>
+        $('#dataTable-1').DataTable({
+            autoWidth: true,
+            "lengthMenu": [
+                [3, 16, 32, 64, -1],
+                [3, 16, 32, 64, "All"]
+            ]
+        });
+
+    </script>
+@endpush
