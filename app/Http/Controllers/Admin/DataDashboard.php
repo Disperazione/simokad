751f813ed7c8247bfdd38c82e86884112c77c4a0
dio selvinus silalebit <dselvinus@gmail.com>
2021-06-03 06:51:46 +0700

    membuat apexchart di dashboard

--- app/Http/Controllers/Admin/DataDashboard.php
+++ app/Http/Controllers/Admin/DataDashboard.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin;
 use App\Models\Guru;
 use App\Models\Kelas;
+use App\Models\Role;
 use App\Models\Siswa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class DataDashboard extends Controller
 {
@@ -18,10 +21,17 @@ class DataDashboard extends Controller
      */
     public function __invoke(Request $request)
     {
+        $jumlah_user = ['Admin' => Admin::all()->count(), 'Siswa' => Siswa::all()->count()];
+        # kalau rolenya single di table guru
+        foreach (Guru::all()->groupBy('role') as $type => $list) {
+            $jumlah_user[Role::where('id', $type)->first()->name] = collect($list)->count();
+        }
+        # kalau rolenya multiple. bikin baru :sob:
         return view('page.dashboard', [
-            'siswa' => Siswa::all()->count(),
+            'siswa' => $jumlah_user['Siswa'],
             'guru' => Guru::all()->count(),
             'kelas' => Kelas::all()->count(),
+            'user'=> Arr::divide($jumlah_user),
         ]);
     }
 }
--- resources/views/page/dashboard.blade.php
+++ resources/views/page/dashboard.blade.php
@@ -80,14 +80,14 @@
 
     {{-- chart --}}
     <div class="row" style="margin-top: 5%">
-        <div class="col-12 col-md-4">
-            <div class="card shadow mb-4">
+        <div class="col-12 col-md-6">
+            <div class="card shadow">
                 <div class="card-header">
-                    <strong class="card-title mb-0">Jumlah user</strong>
+                    <strong class="card-title mb-0">Jumlah User</strong>
                 </div>
-                <div class="card-body text-center">
-                    <div id="multiRadialbar"></div>
-                </div> <!-- / .card-body -->
+                <div class="card-body">
+                    <div id="userDonutChart"></div>
+                </div> <!-- /.card-body -->
             </div> <!-- /.card -->
         </div> <!-- /. col -->
 
@@ -106,7 +106,76 @@
 
     {{-- tutup --}}
 @endsection
+@push('css')
+    <style>
+        .apexcharts-svg{background-color: transparent !important;}
+        .apexcharts-legend{padding: 0 !important;}
+    </style>
+@endpush
 @push('js')
+    <script src="{{ asset('js/apexcharts.min.js') }}"></script>
     <script src="{{ asset('js/apexcharts.custom.js') }}"></script>
-    <script src="{{ asset('js/apexcharts.js') }}"></script>
+    <script>
+        var userDonutChart, userDonutChartOptions = {
+                series: @json($user[1]),
+                labels: @json($user[0]),
+                chart: {
+                    type: "donut",
+                    height: 305,
+                    zoom: {
+                        enabled: !1
+                    },
+                },
+                theme: {
+                    mode: colors.chartTheme,
+                },
+                plotOptions: {
+                    pie: {
+                        donut: {
+                            size: "35%"
+                        },
+                        expandOnClick: !1
+                    }
+                },
+                legend: {
+                    position: "bottom",
+                    fontFamily: base.defaultFontFamily,
+                    fontWeight: 400,
+                    labels: {
+                        colors: colors.mutedColor,
+                        useSeriesColors: !1
+                    },
+                    horizontalAlign: "left",
+                    fontFamily: base.defaultFontFamily,
+                    markers: {
+                        width: 10,
+                        height: 10,
+                        strokeWidth: 0,
+                        strokeColor: "#fff",
+                        radius: 6
+                    },
+                    itemMargin: {
+                        horizontal: 10,
+                        vertical: 2
+                    },
+                    onItemClick: {
+                        toggleDataSeries: !0
+                    },
+                    onItemHover: {
+                        highlightDataSeries: !0
+                    }
+                },
+                stroke: {
+                    colors: [colors.borderColor],
+                    width: 1
+                },
+                fill: {
+                    opacity: 1,
+                    colors: chartColors
+                }
+            },
+            userDonutChartCtn = document.querySelector("#userDonutChart");
+        userDonutChartCtn && (userDonutChart = new ApexCharts(userDonutChartCtn, userDonutChartOptions)).render();
+
+    </script>
 @endpush
